a8e50693acf3a836770a4033d389acadc1637d61
Faisal <faisal_a@live.co.uk>
2017-07-08 15:13:53 +0100

    file structure done in sublime and card class created

---
+++ Blackjack/blackjack_management/Card.java
@@ -0,0 +1,15 @@
+package blackjack_management;
+
+public class Card {
+
+  RankType rank;
+  SuitType suit;
+
+  public Card(RankType rank, SuitType suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  
+
+}
