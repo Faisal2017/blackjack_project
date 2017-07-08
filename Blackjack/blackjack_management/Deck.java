65b9a0ec7f06e10f8e4975c8515155846c12db77
Faisal <faisal_a@live.co.uk>
2017-07-08 15:52:34 +0100

    abstract class Deck created

---
+++ Blackjack/blackjack_management/Deck.java
@@ -0,0 +1,14 @@
+package blackjack_management;
+import java.util.ArrayList;
+
+public abstract class Deck {
+
+  private ArrayList<Card> cards;
+
+  public Deck() {
+    this.cards = new ArrayList<Card>();
+  }
+
+
+
+}
