a1cadee394a8dec4a21c25cab1d66b0399010acb
Faisal <faisal_a@live.co.uk>
2017-07-08 15:16:27 +0100

    rank enum defined

--- Blackjack/blackjack_management/RankType.java
+++ Blackjack/blackjack_management/RankType.java
@@ -1,5 +1,18 @@
 package blackjack_management;
 
 public enum RankType {
+  ACE,
+  Two, 
+  THREE,
+  FOUR, 
+  FIVE, 
+  SIX, 
+  SEVEN, 
+  EIGHT, 
+  NINE, 
+  TEN, 
+  KING,
+  QUEEN,
+  JACK
   
 }
