f448d93185e6ab6c4d1221685e81ace8ce061a80
Faisal <faisal_a@live.co.uk>
2017-07-09 15:31:23 +0100

    player class defined

---
+++ Blackjack/blackjack_management/Player.java
@@ -0,0 +1,15 @@
+package blackjack_management;
+
+public class Player {
+
+  private String name;
+  private ArrayList<Card> hand;
+
+  public Player(String name, ArrayList<Card> hand) {
+    this.name = name;
+    this.hand = hand;
+  }
+
+  
+  
+}
