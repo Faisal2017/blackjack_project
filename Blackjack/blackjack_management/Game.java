5a904c360e15ec99aac7c5c25cef1b8802aadb23
Faisal <faisal_a@live.co.uk>
2017-07-10 14:54:59 +0100

    compare player hands defined in game test class

--- Blackjack/blackjack_management/Game.java
+++ Blackjack/blackjack_management/Game.java
@@ -48,6 +48,5 @@ public class Game {
     player.addCardToHand(dealtCard);
   }
 
-
-
+  
 }
--- Blackjack/blackjack_management_specs/GameTest.java
+++ Blackjack/blackjack_management_specs/GameTest.java
@@ -6,20 +6,20 @@ import java.util.ArrayList;
 public class GameTest {
 
   private Game game;
-  // private Card card1;
-  // private Card card2;
-  // private Card card3;
-  // private Card card4;
+  private Card card1;
+  private Card card2;
+  private Card card3;
+  private Card card4;
   Player player1;
   Player player2;
 
 
   @Before
   public void before() {
-    // card1 = new Card(Rank.KING, Suit.CLUBS);
-    // card2 = new Card(Rank.TWO, Suit.HEARTS);
-    // card3 = new Card(Rank.FIVE, Suit.DIAMONDS);
-    // card4 = new Card(Rank.SEVEN, Suit.SPADES);
+    card1 = new Card(Rank.KING, Suit.CLUBS);
+    card2 = new Card(Rank.TWO, Suit.HEARTS);
+    card3 = new Card(Rank.FIVE, Suit.DIAMONDS);
+    card4 = new Card(Rank.SEVEN, Suit.SPADES);
     player1 = new Player("Player One");
     player2 = new Player("Player Two");
     game = new Game();
@@ -55,6 +55,16 @@ public class GameTest {
     assertEquals(3, result);
   }
 
-  
+  //note player has been given specific cards directly in below test
+  @Test
+  public void comparePlayerHands(player1, player2) {
+    player1.addCardToHand(card1);
+    player1.addCardToHand(card2);
+    player2.addCardToHand(card3);
+    player2.addCardToHand(card4);
+    player1.checkValue(hand);
+    player2.checkValue(hand);
+    assertEquals("Player One wins", game.compareHands(player1, player2));
+  }
 
 }
